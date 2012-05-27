2df099b956d7c34a226172b0100e9db65d8ae4a6
Pronoy Chopra <pronoyc@gmail.com>
2012-05-27 20:26:42 +0530

    updated __init__ to import modules at start

---
+++ profilebuilder/__init__.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+"""
+	Rural Housing Knowledge Networks - Database builder	
+	Author: Pronoy Chopra
+	IIT Delhi
+	
+"""
+
+from __future__ import with_statement
+
+import os
+import pymongo
+from pymongo import Connection
+from bson.objectid import ObjectId, InvalidId
+import pymongoconfig
+from flask import Flask, request, session, redirect, url_for, abort, \
+     render_template, flash, g
+from flask.ext.bcrypt import bcrypt, generate_password_hash, check_password_hash
+import flask_sijax
+
+app = Flask(__name__)
+app.config.from_pyfile('config.cfg')
+
+path = os.path.join('.',os.path.dirname(__file__), '../')
+
+app.config['SIJAX_STATIC_PATH'] = os.path.join('.',os.path.dirname(__file__), 'static/js/sijax')
+
+#initialize flask_sijax
+flask_sijax.Sijax(app)
+
+conn = Connection(pymongoconfig.MONGO_HOST, pymongoconfig.MONGO_PORT)
+dbobj = conn[pymongoconfig.MONGO_DATABASE]
+users = dbobj['accesslist']
+profiles = dbobj['profiles']
+
+
+import profilebuilder.views
