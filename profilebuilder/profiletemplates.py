77b11022461da570dd80efaddfa87763fcaa35f5
Pronoy Chopra <pronoyc@gmail.com>
2012-06-26 11:07:37 +0530

    new template form created

---
+++ profilebuilder/profiletemplates.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask.ext.wtf import TextField, TextAreaField, Form, SubmitField
+
+
+class TemplateSimple(Form):
+	"""
+	Template Simple
+	"""
+	Title = TextField('Profile Title')
+	Unique = TextField('Unique Name')
+	Intro = TextAreaField('Introduction')
+	Design = TextAreaField('Design & Construction')
+	Dos = TextAreaField('Dos & Donts')
+	Materials = TextAreaField('Materials & Labour')
+	Advantages = TextAreaField('Advantages & Limitations')
+	submit = SubmitField('Submit')
